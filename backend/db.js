865e8f3652802246bd4d2c4913664131347d8243
Sankalp-Jain14 <jnsankalp14@gmail.com>
2023-05-21 12:23:24 +0530

    lec 43a

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/';
+
+const connectToMongo = async () => {
+try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB successsfully');
+
+} catch (error){
+    console.log('Error connecting to MongoDB' , error);
+}
+};
+
+module.exports = connectToMongo;

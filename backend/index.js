5f9943b66d0f864337eb99e7341cd1194658031e
Sankalp-Jain14 <jnsankalp14@gmail.com>
2023-05-23 00:03:20 +0530

    lec 45

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,24 @@
 const connectToMongo = require('./db');
 const express = require('express');
 
-connectToMongo();
-const app = express()
 
+const app = express()
 const port = 3000
 
-app.get('/' , (req,res)=>{
-    res.send('Hello World')
-})
+app.use(express.json()) //using middleware to use req.body
+
+// Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 
 app.listen (port,()=>{
     console.log(`Example app listening at http://localhost:${port}`)
+   
 })
+
+connectToMongo();
+
+  
+  
+  

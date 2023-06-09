1646fcecd1d8f74bfdd7f425079676e6d02dd38b
Sankalp-Jain14 <jnsankalp14@gmail.com>
2023-06-10 03:36:48 +0530

    lec60

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,12 +2,71 @@ import React from "react";
 import NoteContext from "./noteContext";
 import { useState } from "react";
 const NoteState = (props) => {
+    const notesInitial = [
+  {
+    "_id": "647c8770ff32c5ee01709448",
+    "user": "647c838411c6dffdedb8d132",
+    "title": "My Title",
+    "description": "Please wake up early",
+    "tag": "personal",
+    "date": "2023-06-04T12:45:36.689Z",
+    "__v": 0
+  },
+  {
+    "_id": "647c8770ff32c5ee0170944a",
+    "user": "647c838411c6dffdedb8d132",
+    "title": "My Title",
+    "description": "Please wake up early",
+    "tag": "personal",
+    "date": "2023-06-04T12:45:36.814Z",
+    "__v": 0
+  },
+  {
+    "_id": "647c8770ff32c5ee0170944c",
+    "user": "647c838411c6dffdedb8d132",
+    "title": "My Title",
+    "description": "Please wake up early",
+    "tag": "personal",
+    "date": "2023-06-04T12:45:36.927Z",
+    "__v": 0
+  },
+  {
+    "_id": "647c8771ff32c5ee0170944e",
+    "user": "647c838411c6dffdedb8d132",
+    "title": "My Title",
+    "description": "Please wake up early",
+    "tag": "personal",
+    "date": "2023-06-04T12:45:37.081Z",
+    "__v": 0
+  },
+  {
+    "_id": "647c8771ff32c5ee01709450",
+    "user": "647c838411c6dffdedb8d132",
+    "title": "My Title",
+    "description": "Please wake up early",
+    "tag": "personal",
+    "date": "2023-06-04T12:45:37.311Z",
+    "__v": 0
+  },
+  {
+    "_id": "647ca825ff32c5ee01709453",
+    "user": "647c838411c6dffdedb8d132",
+    "title": "My Title",
+    "description": "Please access the playlist",
+    "tag": "Youtube",
+    "date": "2023-06-04T15:05:09.089Z",
+    "__v": 0
+  }
+]
+
+const [notes,setNotes] = useState(notesInitial)
+
   return (
-    <NoteContext.Provider value = {{}}>
+    <NoteContext.Provider value = {{notes , setNotes}}>
         {props.children}
     </NoteContext.Provider>
   )
 }
 
-export default NoteState
+export default NoteState;
 
